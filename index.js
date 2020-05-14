675ae85fe5ae105d5e9b6d7dadc60870a74cfe80
Meta1core <howk2000@mail.ru>
2020-05-14 17:50:02 +0300

    maked it better

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const messageRouter = new MessageRouter({
     operatorRoom: io.of('/operator')
 });
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(`${__dirname}/index.html`);
 });
 app.get('/operator', (req, res) => {
